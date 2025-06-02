aae27b65db3514646786cf2a4ccd968af1b123c9
drem-jpg <drem687sawesomeemail@gmail.com>
2025-06-02 06:47:38 -0400

    fix issue in neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,11 @@
 require('config.options')
 require('config.keybinds')
 require("config.lazy")
+function Transparent(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+Transparent()
